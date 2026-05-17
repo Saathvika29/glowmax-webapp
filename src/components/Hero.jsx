1c207f4fd58dde6497a5e180ac94a04641684f0c
Saathvika29 <saathvika.sathu05@gmail.com>
2026-05-17 16:38:47 +0530

    Fix navigation links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
 
-  const navigate = useNavigate();
-
   return (
+
     <div className="hero">
 
       <h1>Level Up Your Lifestyle</h1>
@@ -14,15 +13,13 @@ export default function Hero() {
         all in one place.
       </p>
 
-      <button
-  onClick={() =>
-    <Link to="/signup">
-  <button>Start GlowMax</button>
-</Link>
-  }
->
-  Start GlowMax
-</button>
+      <Link to="/signup">
+
+        <button>
+          Start GlowMax
+        </button>
+
+      </Link>
 
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,28 @@
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
+
   return (
+
     <nav className="navbar">
 
       <h2>GlowMax</h2>
 
       <ul>
+
         <li>Home</li>
         <li>Features</li>
         <li>Support</li>
-        <button
-          onClick={() =>
-            <Link to="/login">
-  <button>Login</button>
-</Link>
-          }
-        >
-          Login
-        </button>
 
-        <button
-          onClick={() =>
-           <Link to="/signup">
-  <button>Signup</button>
-</Link>
-          }
-        >
-          Signup
-        </button>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+        <Link to="/signup">
+          <button>Signup</button>
+        </Link>
+
       </ul>
-      
 
     </nav>
   );
